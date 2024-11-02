b156a6227e7c72ab08b7d887fc69322f56483575
Vilius Paskonis <vilius.paskonis@gmail.com>
2024-11-02 22:00:32 +0200

    Extract function variable

--- src/Painter.java
+++ src/Painter.java
@@ -13,8 +13,10 @@ class Painter extends JPanel {
 
         int a = 1;
         for (double i = -100; i < 100; i += Constants.DOT_DENSITY) {
+            double function = a * i * Math.exp(1-i);
+
             int x = (int)(i * Constants.ZOOM_LEVEL + Constants.FRAME_WIDTH / 2);
-            int y = Constants.FRAME_HEIGHT - (int)(a * i * Math.exp(1-i) * Constants.ZOOM_LEVEL + Constants.FRAME_HEIGHT / 2) - 5;
+            int y = Constants.FRAME_HEIGHT - (int)(function * Constants.ZOOM_LEVEL + Constants.FRAME_HEIGHT / 2) - 5;
 
             g.fillOval(x, y, 5, 5);
         }
