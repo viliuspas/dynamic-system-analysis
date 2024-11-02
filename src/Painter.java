4b9998312a4da36a0f2ce7f61d6088277ef7b385
Vilius Paskonis <vilius.paskonis@gmail.com>
2024-11-03 01:01:54 +0200

    Painter constants clean up

--- src/Painter.java
+++ src/Painter.java
@@ -2,7 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 
 class Painter extends JPanel {
-
+    final int fHeight = Constants.FRAME_HEIGHT;
+    final int fWidth = Constants.FRAME_WIDTH;
+    final double domainMin = Constants.DOMAIN_MIN;
+    final double domainMax = Constants.DOMAIN_MAX;
+    final double dotDensity = Constants.DOT_DENSITY;
+    final int zoom = Constants.ZOOM_MULTIPLIER;
+    final int maxJump = Constants.MAX_JUMP;
     private String inputText = "1";
 
     public void setInputText(String inputText){
@@ -17,21 +23,22 @@ class Painter extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        // x
-        g.drawLine(0, Constants.FRAME_HEIGHT / 2, Constants.FRAME_WIDTH, Constants.FRAME_HEIGHT / 2);
-        // y
-        g.drawLine(Constants.FRAME_WIDTH / 2, 0, Constants.FRAME_WIDTH / 2, Constants.FRAME_HEIGHT);
+        // x axis
+        g.drawLine(0, fHeight / 2, fWidth, fHeight / 2);
+        // y axis
+        g.drawLine(fWidth / 2, 0, fWidth / 2, fHeight);
 
         double a = Double.parseDouble(getInputText());
         Integer lastX = null;
         Integer lastY = null;
 
         g.setColor(Color.blue);
-        for (double i = Constants.DOMAIN_MIN; i < Constants.DOMAIN_MAX; i += Constants.DOT_DENSITY) {
+
+        for (double i = domainMin; i < domainMax; i += dotDensity) {
             double function = i * Math.exp(a * (1 - i));
 
-            int x = (int)(i * Constants.ZOOM_MULTIPLIER + Constants.FRAME_WIDTH / 2);
-            int y = Constants.FRAME_HEIGHT - (int)(function * Constants.ZOOM_MULTIPLIER + Constants.FRAME_HEIGHT / 2);
+            int x = (int)(i * zoom + fWidth / 2);
+            int y = fHeight - (int)(function * zoom + fHeight / 2);
 
             if(lastX == null){
                 lastX = x;
@@ -40,7 +47,7 @@ class Painter extends JPanel {
 
             // ensures that the distance between points isn't too big
             // avoids calculation / drawing errors
-            if(Math.abs(x - lastX) < Constants.MAX_JUMP && Math.abs(y - lastY) < Constants.MAX_JUMP){
+            if(Math.abs(x - lastX) < maxJump && Math.abs(y - lastY) < maxJump){
                 g.drawLine(lastX, lastY, x, y);
             }
 
